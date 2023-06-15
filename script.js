1dcabc37e98c1bba498ba79c08e6618e9766b1f7
unknown <manoharmakkena717@gmail.com>
2023-06-15 21:49:19 +0530

    apdated about section

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+      $('.menu-botton').click(function(){
+          $('.navbar .menu').toggleClass("active");
+          $('.menu-botton i').toggleClass("active");
+      });
+});
